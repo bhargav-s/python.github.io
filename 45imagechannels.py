10a41cf019deb468207def8c5f42d182157e29f8
bhargav <bhargavs2493@gmail.com>
2017-02-08 09:58:38 +0530

    new ones

--- 45imagechannels.py
+++ 45imagechannels.py
@@ -1,12 +1,14 @@
 from  PIL import Image
 
 test = Image.open("Activity.PNG")
-test2 = Image.open("Activity.PNG")
+test2 = Image.open("class.PNG")
 print(test.mode)
 r1, g1, b1, a1 = test.split()
 r2, g2, b2, a2 = test2.split()
 
 new_img = Image.merge("RGBA",(a1, g2, r1, b1))
 new_img1 = test.transpose(Image.FLIP_TOP_BOTTOM)
+
+
 #new_img.show()
 new_img1.show()
